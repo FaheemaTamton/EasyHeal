44566890ee624f7b065abce82a9fdffc4b38292c
Faheema Tamton <fymatampc@gmail.com>
2026-06-05 11:11:49 +0530

    Update test_preprocess.py

--- test_preprocess.py
+++ test_preprocess.py
@@ -3,7 +3,6 @@ from backend.vlm.preprocess import preprocess_prescription
 
 processed = preprocess_prescription("test_crop.jpg")
 
-cv2.imwrite("debug_processed.jpg", processed)
-print("Saved debug_processed.jpg")
+
 
 
